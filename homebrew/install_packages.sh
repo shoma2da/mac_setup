f917d9a3174a095d3e2582506bcd8d2c881d127b
shoma2da <marcos.shoichi.matsuda@gmail.com>
2024-11-05 19:22:25 +0900

    add warp

--- homebrew/install_packages.sh
+++ homebrew/install_packages.sh
@@ -5,7 +5,13 @@ if ! command -v brew &> /dev/null; then
     exit 1
 fi
 
-brew install --cask 1password
 brew install --cask arc
+
 brew install --cask karabiner-elements
+
 brew install --cask raycast
+
+brew install --cask 1password
+brew install 1password-cli
+
+brew install --cask warp
