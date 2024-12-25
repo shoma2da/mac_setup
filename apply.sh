a702c4ac01a2e02ab21cd6ae7b509461bf7354f7
shoma2da <marcos.shoichi.matsuda@gmail.com>
2024-12-26 00:37:15 +0900

    update files

--- apply.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-launchctl unload $HOME/Library/LaunchAgents/setup.plist
-launchctl load $HOME/Library/LaunchAgents/setup.plist
--- uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-echo "remove setup agent files."
-rm $HOME/Library/LaunchAgents/setup.plist
-rm $HOME/setup.sh
-
